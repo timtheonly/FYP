cdddcc18fda59a5adcc0f0e6f9ebc12c7b01cc4a
Daniel hogan <timtheonly@gmail.com>
2014-01-26 20:33:19 +0000

    (WIP) ADD ROUTE TO LIST ALL SESSIONS
    get /session/

--- routes/sessions.js
+++ routes/sessions.js
@@ -7,18 +7,19 @@ module.exports.setup = function(app, mongoose){
 	/*
 	 * Session routes
 	 */
-	 var Session = mongoose.model('session');
-	 var baseUrl = '/session'
+	var Session = mongoose.model('session');
+	var baseUrl = '/session';
 
-	 //list all sessions
-	 app.get(baseUrl,function(req,res){
-
-	 	res.send('hello');
-
-	 });
+	//list all sessions
+	app.get(baseUrl,function(req,res){
+		Session.getAll(function(err,sessions){
+			if(err){throw err;}
+			res.send(sessions);
+		});
+	});
 
 	 //create a new session
-	 app.post(baseUrl,function(req,res){
-	 	res.send('hello');
-	 });
+	app.post(baseUrl,function(req,res){
+		res.send('hello');
+	});
 };
