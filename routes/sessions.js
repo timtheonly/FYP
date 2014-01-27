859937c22341181994d2e3a76bb6ed078fa7cab8
Daniel hogan <timtheonly@gmail.com>
2014-01-27 22:07:06 +0000

    ADD ROUTE TO GET INDIVIDUAL SESSION
    /session/:id

--- routes/sessions.js
+++ routes/sessions.js
@@ -18,6 +18,20 @@ module.exports.setup = function(app, mongoose){
 		});
 	});
 
+	//get 
+	app.get(baseUrl +'/:id',function(req,res){
+		Session.findOne({_id:req.params.id},function(err,session){
+			
+			if(err){throw err;}
+
+			if(session !== null){
+				res.send(session);
+			}else{
+				res.send('session not found');
+			}
+		});
+	});
+
 	 //create a new session
 	app.post(baseUrl,function(req,res){
 		res.send('hello');
