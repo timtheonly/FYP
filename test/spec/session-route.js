3580b61a01e687c05e5b84c39ed32350d23e804d
Daniel hogan <timtheonly@gmail.com>
2014-02-05 17:29:44 +0000

    CREATE TESTS FOR SESSION ROUTES

--- test/spec/session-route.js
+++ test/spec/session-route.js
@@ -38,4 +38,74 @@ describe('#routes test /session', function(){
 			});
 	});
 
+	it('should list a single session',function(done){
+		http.get('http://localhost:9000/session/'+sessionID+'/')
+			.end(function(err,res){
+				if(err){done(err);}
+				expect(res.body.name).to.equal('supper awesome poll');
+				done();
+			});
+	});
+
+	it('should create a session', function(done){
+		var tempSessionId;
+		http.post('http://localhost:9000/session')
+		.send({
+			name: 'supper awesome poll',
+			tags: ['blah','blah','blah'],
+			creator:'52e6d0174d61ba401d00004b',
+			open: true
+		})
+		.end(function(res){
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			tempSessionId = res.body;
+			http.del('http://localhost:9000/session/'+tempSessionId +'/')
+			.end(function(){
+				done();
+			});
+		});
+	});
+
+	it('should remove a session', function(done){
+		var tempSessionId;
+		http.post('http://localhost:9000/session')
+		.send({
+			name: 'supper awesome poll',
+			tags: ['blah','blah','blah'],
+			creator:'52e6d0174d61ba401d00004b',
+			open: true
+		})
+		.end(function(res){
+			tempSessionId = res.body;
+			http.del('http://localhost:9000/session/'+tempSessionId +'/')
+			.end(function(res2){
+				expect(res2).to.exist;
+				expect(res2.status).to.equal(200);
+				expect(res2.text).to.equal('session deleted');
+				done();
+			});
+		});
+	});
+
+	it('should allow a session to be opened',function(done){
+		http.put('http://localhost:9000/session/'+sessionID+'/open/').
+		end(function(res){
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			expect(res.text).to.equal('session opened');
+			done();
+		});
+	});
+
+	it('should allow a session to be closed',function(done){
+		http.put('http://localhost:9000/session/'+sessionID+'/close/').
+		end(function(res){
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			expect(res.text).to.equal('session closed');
+			done();
+		});
+	});
+
 });
