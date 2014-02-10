4420485dbb14a3fd4a692949ffc209925759951a
Daniel hogan <timtheonly@gmail.com>
2014-02-10 12:44:34 +0000

    TITLE IS NOW MODIFIED WHEN PAGE IS ROUTED

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,9 +8,17 @@ angular.module('fypApp', [
   'ui.bootstrap'
 ]).config(['$routeProvider', function($routeProvider){
 	$routeProvider
-		.when('/home',{templateUrl:'../partials/home.html'})
+		.when('/home',{
+			templateUrl:'../partials/home.html',
+			title:'home'
+		})
 		.when('/sessions',{
 			templateUrl: '../partials/sessions.html',
-			controller:'sessionsCtrl'
+			controller:'sessionsCtrl',
+			title:'Session'
 		});
-}]);
+}]).controller('titleCtrl', function($scope,$route){
+	$scope.$on('$routeChangeSuccess',function(){
+		$scope.title = $route.current.title;
+	});
+});
--- app/scripts/controllers/session.js
+++ app/scripts/controllers/session.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('fypApp').controller('sessionsCtrl',function($scope){
+	$scope.$root.page_title = 'Session';
 	$scope.message = 'hello';
 });
