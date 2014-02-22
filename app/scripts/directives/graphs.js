2a714edcba209ca3511274890264ebbdd1ceca53
Daniel hogan <timtheonly@gmail.com>
2014-02-22 20:32:44 +0000

    ADD DIRECTIVES FOR BAR GRAPH AND PIE CHART
    both using remote data

--- app/scripts/directives/graphs.js
+++ app/scripts/directives/graphs.js
@@ -2,7 +2,7 @@
 /* Idea for directive based method from
  * http://briantford.com/blog/angular-d3.html
  */
-angular.module('fypApp').directive('d3BarGraph',function(){
+angular.module('fypApp').directive('d3BarGraph',function(){//bar graph
 	return{
 		restrict:'E',
 		scope:{
@@ -12,33 +12,78 @@ angular.module('fypApp').directive('d3BarGraph',function(){
 		},
 		link:
 		function(scope, element){
-			element.append('<svg class="chart"width="'+scope.width+'" height="'+scope.height+'"></svg>');
+			element.append('<svg class="chart" width="'+scope.width+'" height="'+scope.height+'"></svg>');
 			/* Based on example from
 			*  http://nvd3.org/ghpages/discreteBar.html
 			*/
 			var chart;
-			nv.addGraph(function(){
-				chart = nv.models.discreteBarChart()
-				.x(function(d){
-					return d.answer;
-				})
-				.y(function(d){
-					return d.response;
-				})
-				.staggerLabels(false)
-				.showValues(false)
-				.width(scope.width)
-				.height(scope.height);
+            scope.$watch('values', function(newVal){
+               if(!newVal)
+               {
+                   return;
+               }
+               nv.addGraph(function(){
+                    chart = nv.models.discreteBarChart()
+                        .x(function(d){
+                            return d.answer;
+                        })
+                        .y(function(d){
+                            return d.response;
+                        })
+                        .staggerLabels(false)
+                        .showValues(false)
+                        .width(scope.width)
+                        .tooltips(false)
+                        .height(scope.height);
 
-				d3.select('.chart')
-				.datum([
-					{
-						values:[{'answer':'yes', 'response':9}, {'answer':'no', 'response':10}]
-					}])
-				.call(chart);
+                    d3.select('.chart')
+                        .datum([
+                            {
+                                values:scope.values
+                            }])
+                        .call(chart);
 
-				return chart;
-			});
+                    return chart;
+                });
+            });
+
 		}
 	};
+})
+.directive('d3PieChart',function(){
+    return{
+        restrict:'E',
+        scope:{
+            values :'=',
+            width:'=',
+            height:'='
+        },
+        link:function(scope,element){
+            element.append('<svg class="chart" width="'+scope.width+'" height="'+scope.height+'"></svg>');
+            /* Based on example from
+             *  http://nvd3.org/ghpages/discreteBar.html
+             */
+            var chart;
+            scope.$watch('values', function(newVal){
+                if(!newVal)
+                {
+                    return;
+                }
+                nv.addGraph(function(){
+                    chart = nv.models.pieChart()
+                        .x(function(d){return d.answer;})
+                        .y(function(d){ return d.response;})
+                        .tooltips(false)
+                        .width(scope.width)
+                        .height(scope.height);
+
+                    d3.select('.chart')
+                        .datum(scope.values)
+                        .call(chart);
+
+                    return chart;
+                });
+            });
+        }
+    }
 });
